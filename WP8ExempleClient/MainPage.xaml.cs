ff7361f82e0d0ea117907f49780b524ef473dccf
ThierryTournier <xuanthierry@gmail.com>
2013-09-16 11:19:03 +0200

    Interface WP
    Interface WP ok, reste à porter la hapi

--- WP8ExempleClient/MainPage.xaml.cs
+++ WP8ExempleClient/MainPage.xaml.cs
@@ -8,34 +8,118 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using WP8ExempleClient.Resources;
+using HubiquitusDotNetPortable.hapi.hStructures;
+using System.Diagnostics;
+using HubiquitusDotNetWP8.hapi.client;
+using Newtonsoft.Json.Linq;
 
 namespace WP8ExempleClient
 {
     public partial class MainPage : PhoneApplicationPage
     {
+
+        private HClient client;
+        private HOptions options;
+
         // Constructeur
         public MainPage()
         {
-            InitializeComponent();
+            InitializeComponent();          
+            options = new HOptions();
 
-            // Exemple de code pour la localisation d'ApplicationBar
-            //BuildLocalizedApplicationBar();
+            client = new HClient();
+            client.onMessage += client_onMessage;
+            client.onStatus += client_onStatus;
+        }
+        void client_onStatus(HStatus status)
+        {
+            Debug.WriteLine(">>>client_onStatus: " + status.ToString());
+            Debug.WriteLine("--> fulljid : " + client.FullJid);
+            Debug.WriteLine("--> resource : " + client.Resource);
+            Update_TextBlock_UI(tbStatusScreen, status.ToString());
         }
 
-        // Exemple de code pour la conception d'une ApplicationBar localisée
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Définit l'ApplicationBar de la page sur une nouvelle instance d'ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
+        void client_onMessage(HMessage message)
+        {
+            Debug.WriteLine(">>>client_onMessage: " + message.ToString());
+            Update_TextBlock_UI(tbMessageScreen, ">>>onMessage<<< \n" + message.ToString());
+        }
+        private void callback(HMessage msg)
+        {
+            Debug.WriteLine(">>>[Callback]<<< \n " + msg.ToString() + "\n");
+            Update_TextBlock_UI(tbMessageScreen, ">>>CallBack<<< \n" + msg.ToString());
+        }
+        private void btConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (options.GetEndpoints() != null)
+                options.GetEndpoints().Clear();
 
-        //    // Crée un bouton et définit la valeur du texte sur la chaîne localisée issue d'AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
+            string endpoint = tbServer.Text;
+            if (!string.IsNullOrEmpty(endpoint))
+            {
+                JArray ja = new JArray();
+                ja.Add(endpoint);
+                options.SetEndpoints(ja);
+            }
 
-        //    // Crée un nouvel élément de menu avec la chaîne localisée d'AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+            client.Connect(tbUserName.Text, tbPassword.Text, options);
+        }
+
+        private void btDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            client.Disconnect();
+        }
+
+        private void btClearConnect_Click(object sender, RoutedEventArgs e)
+        {
+            Update_TextBlock_UI(tbStatusScreen, "clear");
+        }
+        
+
+        private void btSubscribe_Click(object sender, RoutedEventArgs e)
+        {
+            client.Subscribe(tbActor.Text, callback);
+        }
+
+        private void btUnsbscribe_Click(object sender, RoutedEventArgs e)
+        {
+            client.Unsubscribe(tbActor.Text, callback);
+        }
+
+        private void btSend_Click(object sender, RoutedEventArgs e)
+        {
+            HMessageOptions mOptions = new HMessageOptions();
+
+            //if (persistentCb.IsChecked.Value)
+            //    mOptions.Persistent = true;
+            //else
+            //    mOptions.Persistent = false;
+
+            //if (!string.IsNullOrEmpty(timeoutTbx.Text))
+            //    mOptions.Timeout = int.Parse(timeoutTbx.Text);
+
+            //if (!string.IsNullOrEmpty(relevantTbx.Text))
+            //    mOptions.RelevanceOffset = int.Parse(relevantTbx.Text);
+
+            HMessage hMsg = client.BuildMessage(tbActor.Text, "string", tbMessage.Text, mOptions);
+            client.Send(hMsg, null);
+
+            Debug.WriteLine(">>>Send Message<<<\n" + hMsg.ToString() + "\n");
+            Debug.WriteLine(">>>BareJid<<<\n" + client.BareJid + "\n");
+
+        }
+
+        private void btClearMessage_Click(object sender, RoutedEventArgs e)
+        {
+            Update_TextBlock_UI(tbMessageScreen, "clear");
+        }
+
+        private void Update_TextBlock_UI(TextBlock tb, string text)
+        {
+            if ("clear".Equals(text, StringComparison.OrdinalIgnoreCase))
+                tb.Text = "";
+            else
+                tb.Text += text;
+        }
     }
 }
